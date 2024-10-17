63f72f6986e7ee3d232b913cf11fe096de7b54cc
leticialima05 <159441052+leticialima05@users.noreply.github.com>
2024-10-17 10:58:32 -0300

    Add files via upload

---
+++ desafi02IA.js
@@ -0,0 +1,16 @@
+const imagem = document.getElementById('imagem');
+const proximoBtn = document.getElementById('proximo');
+const anteriorBtn = document.getElementById('anterior');
+
+let imagens = ['cadeira.jpeg', 'taça.jpeg', 'rosto.jpeg']; // Adicione mais imagens aqui
+let indiceImagem = 0;
+
+proximoBtn.addEventListener('click', () => {
+    indiceImagem = (indiceImagem + 1) % imagens.length;
+    imagem.src = imagens[indiceImagem];
+});
+
+anteriorBtn.addEventListener('click', () => {
+    indiceImagem = (indiceImagem - 1 + imagens.length) % imagens.length;
+    imagem.src = imagens[indiceImagem];
+});
